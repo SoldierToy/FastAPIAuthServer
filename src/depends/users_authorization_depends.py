53f321dfb805548b27d13d10b9f88f58ac68698d
soldiertoy <soldiertoy01@gmail.com>
2023-11-27 21:17:28 +0700

    add RefreshTokenValidate depends

--- src/depends/users_authorization_depends.py
+++ src/depends/users_authorization_depends.py
@@ -1,14 +1,17 @@
 from datetime import datetime
-
+from collections import namedtuple
 import jose
 from src.jwt.generate_jwt import TokenCreator
-from fastapi import Header, HTTPException
-from src.schemas.users_schemas import UserAuthTokenSchema
+from fastapi import Header, HTTPException, Cookie
+from src.schemas.users_schemas import UserAuthTokenSchema, UserAuthRefreshTokenSchema
 
 token_exception = HTTPException(status_code=401, detail='invalid token')
 
+RefreshTokenData = namedtuple('RefreshTokenData', ['token_data', 'refresh_token'])
 
-class CheckUserAuth:
+
+class TokenValidate:
+    """Валидация токена. Парсинг и проверка expires"""
 
     @staticmethod
     def parse_jwt(token, parser=TokenCreator.decode_tokens):
@@ -21,16 +24,37 @@ class CheckUserAuth:
         if time_now > token_expire:
             raise token_exception
 
+
+class CheckUserToken:
+
     def __call__(self, token=Header(alias='x-authorization', default=None)):
         if token is None:
             raise token_exception
         try:
-            print(CheckUserAuth.parse_jwt(token))
-            user_token_date = UserAuthTokenSchema(**CheckUserAuth.parse_jwt(token))
-            CheckUserAuth.check_expire_token(user_token_date.exp)
+            user_token_data = UserAuthTokenSchema(**TokenValidate.parse_jwt(token))
+            TokenValidate.check_expire_token(user_token_data.exp)
         except jose.exceptions.JWTError:
             raise token_exception
-        return user_token_date
+        return user_token_data
 
 
-check_user_auth = CheckUserAuth()
+check_user_jwt = CheckUserToken()
+
+
+class RefreshTokenValidate:
+    def __call__(self, refresh_token=Cookie(alias='refresh_token', default=None)):
+        if refresh_token is None:
+            raise token_exception
+        try:
+            token_dict = TokenValidate.parse_jwt(refresh_token)
+            token_dict['token'] = refresh_token
+
+            token_data = UserAuthRefreshTokenSchema(**token_dict)
+            TokenValidate.check_expire_token(token_data.exp)
+        except jose.exceptions.JWTError:
+            raise token_exception
+
+        return token_data
+
+
+check_user_refresh_token = RefreshTokenValidate()
