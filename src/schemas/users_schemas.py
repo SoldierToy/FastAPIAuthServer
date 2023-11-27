5dba61bbcc64c1cd855db49d1d3d766010c1739e
soldiertoy <soldiertoy01@gmail.com>
2023-11-27 18:56:18 +0700

    changes users schemas

--- src/schemas/users_schemas.py
+++ src/schemas/users_schemas.py
@@ -1,7 +1,5 @@
-from uuid import UUID, uuid4
-
-from pydantic import BaseModel
-from pydantic import UUID4, EmailStr, Field
+from pydantic import BaseModel, UUID4
+from pydantic import EmailStr, Field
 
 
 class CreateUserSchema(BaseModel):
@@ -11,14 +9,23 @@ class CreateUserSchema(BaseModel):
     email: EmailStr
 
 
+class UpdateUserSchema(BaseModel):
+    id: str
+    tg_id: int | None = Field(gt=1, default=None)
+    name: str | None = Field(max_length=32, default=None)
+    password: str | None = Field(max_length=64, default=None)
+    email: EmailStr | None = Field(default=None)
+    role: str = Field(default=None)
+
+
 class AuthUserSchema(BaseModel):
     email: EmailStr
     password: str
 
 
 class UserAuthTokenSchema(BaseModel):
-    name: str
-    email: str
-    uuid: str
-    role: str
-    exp: int
+    id: str
+    name: str = Field(default=None)
+    email: str = Field(default=None)
+    role: str = Field(default=None)
+    exp: int = Field(default=None)
