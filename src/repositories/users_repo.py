0d69140ab2f0cfcb333519c8fced8506a03e2bd2
soldiertoy <soldiertoy01@gmail.com>
2023-11-27 21:17:40 +0700

    add delete_refresh_token_for_user

--- src/repositories/users_repo.py
+++ src/repositories/users_repo.py
@@ -1,6 +1,6 @@
 from src.db.connection import async_session_factory
 from src.models import Users, RefreshTokens
-from sqlalchemy import insert, select, update
+from sqlalchemy import insert, select, update, delete
 
 
 class UsersRepo:
@@ -40,7 +40,7 @@ class UsersRepo:
 
     async def add_refresh_token_for_user(self, user_id: int, refresh_token: str):
         async with async_session_factory() as session:
-            stmt = insert(self.model_refresh_tokens).values(user_fk=user_id, refresh_token=refresh_token)
+            stmt = insert(self.model_refresh_tokens).values(user_id=user_id, refresh_token=refresh_token)
             await session.execute(stmt)
             await session.commit()
 
@@ -48,5 +48,11 @@ class UsersRepo:
         async with async_session_factory() as session:
             user_id = data.get('id')
             stmt = update(self.model_user).filter_by(id=user_id).values(**data)
+            await session.execute(stmt)
+            await session.commit()
+
+    async def delete_refresh_token_for_user(self, refresh_token):
+        async with async_session_factory() as session:
+            stmt = delete(self.model_refresh_tokens).filter_by(refresh_token=refresh_token)
             await session.execute(stmt)
             await session.commit()
