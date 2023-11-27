27c06497aa5a41fa624f0c2bbfe51e57cd7ad187
soldiertoy <soldiertoy01@gmail.com>
2023-11-27 14:52:22 +0700

    create depends class CheckUserAuth

---
+++ src/depends/users_authorization_depends.py
@@ -0,0 +1,23 @@
+import jose
+from src.jwt.generate_jwt import TokenCreator
+from fastapi import Header, HTTPException
+from src.schemas.users_schemas import UserAuthToken
+
+
+class CheckUserAuth:
+
+    @staticmethod
+    def parse_jwt(token, parser=TokenCreator.decode_tokens):
+        return parser(token)
+
+    def __call__(self, token=Header(alias='x-authorization', default=None)):
+        if token is None:
+            raise HTTPException(status_code=401, detail='invalid token')
+        try:
+            user_token_date = UserAuthToken(**CheckUserAuth.parse_jwt(token))
+        except jose.exceptions.JWTError:
+            raise HTTPException(status_code=401, detail='invalid token')
+        return user_token_date
+
+
+check_user_auth = CheckUserAuth()
