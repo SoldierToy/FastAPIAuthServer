bddd9836c507787e8c421227606a7aaae5ecf35f
soldiertoy <soldiertoy01@gmail.com>
2023-11-27 14:59:33 +0700

    add test auth endpoint

--- src/api_v1/auth/auth.py
+++ src/api_v1/auth/auth.py
@@ -1,7 +1,9 @@
 from collections import namedtuple
 
 from fastapi import APIRouter, Depends, Response
-from src.schemas.users_schemas import CreateUserSchema, AuthUserSchema
+
+from src.depends.users_authorization_depends import check_user_auth
+from src.schemas.users_schemas import CreateUserSchema, AuthUserSchema, UserAuthToken
 from src.depends.users_service_depends import users_service_depends
 
 router = APIRouter(tags=["auth"])
@@ -16,11 +18,17 @@ async def register_user(
 
 @router.post('/authenticated')
 async def authenticated(response: Response, user: AuthUserSchema, user_service=Depends(users_service_depends)):
-    tokens: namedtuple = await user_service.authenticated(user)
-    response.set_cookie('auth_token', tokens.refresh_token, httponly=True)
+    tokens: namedtuple = await user_service.authenticated_user(user)
+    response.set_cookie('refresh_token', tokens.refresh_token, httponly=True)
     return {'access_token': tokens.access_token}
 
 
 @router.post('/update_access_token')
 async def update_access_token():
     pass
+
+
+@router.post('/private_endpoint')
+async def private(user_token_date: UserAuthToken = Depends(check_user_auth)):
+    print(user_token_date.email)
+    return {'post': 'private_endpoint'}
